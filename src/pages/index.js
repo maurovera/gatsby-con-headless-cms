542d33cf05f51cd31ffb211919a2a9e86397fcf8
maurovera <mau4250@gmail.com>
2021-05-29 17:40:56 -0400

    fixed de purge

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,8 +24,8 @@ const IndexPage = (props) => {
         style={{ marginBottom: `1.45rem` }}
       />
       <p>
-        <Link to="/page-2/">Go to page 2</Link> <br />
-        <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+        <Link to="/">Go to page 2</Link> <br />
+       
       </p>
   
     </Layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  purge: ["./src/components/**/*.{js, jsx, ts, tsx}", "./src/pages/**/*.{js, jsx, ts, tsx}"],
 
+  purge: [],
+         
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
